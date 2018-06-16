e8ec4e6621c7d792eaeb111ee26f35461a10c55d
Alex Turnbull <alexturnbull@alexs-macbook-pro.local>
2018-06-16 19:59:37 +0100

    Initial Commit

---
+++ LearnToWeek7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnToWeek7
+//
+//  Created by Alex Turnbull on 16/06/2018.
+//  Copyright © 2018 Alex Turnbull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
